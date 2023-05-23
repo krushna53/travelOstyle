09b9e6816fb545ad6127c7e192d53ed948dc249d
sagarbharvadiya <sagarbharvadiya@gmail.com>
2023-05-23 19:24:21 +0530

    banner section

--- src/Components/BannerNew.jsx
+++ src/Components/BannerNew.jsx
@@ -20,23 +20,33 @@ const BannerNew = () => {
       }
     };
     fetchPage();
+    
   }, []);
   return (
     <>
-      <section className="banner_new">
+      <section className="banner_new ">
+        <div className="same_width">
         <div className="Banner_content">
           {entry.map((item, i) => {
             const { title, description } = item.fields;
+            const imageUrl = (item?.fields?.image?.fields?.file?.url) ? item?.fields?.image?.fields?.file?.url : '';
             const id = item.sys.id
             return (
               <React.Fragment key={id}>
+                <div className="left">
                 <h2>{title}</h2>
                 <p>{description}</p>
                 <button className="btn">Get Details</button>
+                </div>
+                <div className="right">
+                  <img src={imageUrl} alt="imageNew" />
+                </div>
               </React.Fragment>
             );
           })}
         </div>
+        </div>
+       
       </section>
     </>
   );
