6e5d5333115143b2d05a67858dd277488448fe67
sagarbharvadiya <sagarbharvadiya@gmail.com>
2023-05-24 19:38:32 +0530

    banner-section

--- src/Components/BannerNew.jsx
+++ src/Components/BannerNew.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import client from "../Client";
-import Image from '../images/image (2).png'
 const BannerNew = () => {
   const [entry, setEntry] = useState([]);
 
@@ -24,51 +23,34 @@ const BannerNew = () => {
   return (
     <>
       <section className="banner_new ">
-        {/* <div className="same_width">
-          <div className="Banner_content">
+        <div className="banner_com same_width">
+          <div className="d_flex">
             {entry.map((item, i) => {
-              const { title, description } = item.fields;
+              const { title, description, leftTitle, rightTitle, linkField } =
+                item.fields;
               const imageUrl = item?.fields?.image?.fields?.file?.url
                 ? item?.fields?.image?.fields?.file?.url
                 : "";
               const id = item.sys.id;
               return (
                 <React.Fragment key={id}>
-                  <div className="left">
-                    <h2>{title}</h2>
-                    <p>{description}</p>
-                    <button className="btn">Get Details</button>
+                  <div className="banner_left_con">
+                    <p className="red">{title}</p>
+                    <h1 className="title">
+                      {leftTitle} <span>O</span> {rightTitle}
+                    </h1>
+                    <div className="dec">{description}</div>
+                    <div className="banner_btn">
+                      <button>{linkField}</button>
+                    </div>
                   </div>
-                  <div className="right">
-                    <img src={imageUrl} alt="imageNew" />
+                  <div className="sc-ieecCq fIygvt">
+                    <img src={imageUrl} alt="img" />
                   </div>
                 </React.Fragment>
               );
             })}
           </div>
-        </div> */}
-
-        {/*  */}
-        <div className="banner_com">
-        <div class="sc-bkkeKt gGCbNn">
-          <div class="banner_left_con">
-            <p class="red">Best Destination Around The World</p>
-            <h1 class="title">
-              Travel, <span>enjoy</span> and live a new and full life
-            </h1>
-            <p class="dec">
-              Built Wicket longer admire do barton vanity itself do in it.
-              Preferred to sportsmen it engrossed listening. Park gate sell they
-              west hard for the.
-            </p>
-            <div class="sc-fKVqWL eAXuhA">
-              <button>Find out more</button>
-            </div>
-          </div>
-          <div class="sc-ieecCq fIygvt">
-            <img src={Image} alt="img" />
-          </div>
-        </div>
         </div>
       </section>
     </>
